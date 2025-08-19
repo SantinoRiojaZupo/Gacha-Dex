cbacf8f0e8a69b5cabe728e583b7c4da4fe22120
SantinoRiojaZupo <santinoriojazupo@gmail.com>
2025-08-19 12:48:07 -0300

    Subo parte de los sitios , sin terminar

---
+++ Sitio.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="src/pages/styles.css" />
+    <title>Sobre Nosotros - Lazarte</title>
+  </head>
+  <body>
+    <header>
+      <nav class="nav" id="nav1">
+        <ul>
+          <li><a href="inicio.html">Inicio</a></li>
+          <li><a href="servicios.html">Servicios</a></li>
+          <li><a href="Portafolio.html">Portafolio</a></li>
+          <li><a href="SobreNosotros.html">Sobre Nosotros</a></li>
+          <li><a href="Testimonio.html">Testimonios</a></li>
+          <li><a href="Contacto.html">Contacto</a></li>
+        </ul>
+      </nav>
+    </header>
+
+    <main class="about">
+      <section class="about-content">
+        <div class="about-text">
+          <h2>¿Quiénes Somos?</h2>
+          <p>
+            En <strong>PokeGarcha</strong>, somos un Gacha de pokemon
+          </p>
+          <p>
+            Nuestro equipo está formado por emprendedores jóvenes, comprometidos
+            con la calidad, la innovación y el crecimiento de nuestros clientes.
+            Acompañamos cada proyecto con visión estratégica, cercanía y
+            resultados.
+          </p>
+          <p><em>Tu éxito también es el nuestro.</em></p>
+        </div>
+
+        <div class="about-image">
+          <img
+            src="img/"
+            alt="Fundadores de PokeGarcha: Benja"
+          />
+          <p class="caption">Alejandro (Cuello) y Santino Rioja Zupo, cofundadores de Pokegarcha</p>
+        </div>
+      </section>
+    </main>
+
+    <footer>
+      <p>&copy; 2025 LosAmiguitos. Todos los derechos reservados.</p>
+    </footer>
+</body>
+</html>
---
+++ sitio.html
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Gacha-dex</title>
+    <link rel="icon" type="image/x-icon" href="gachadex.png">
+</head>
+<body>
+    <header>
+        <ul>
+            <li><a href="sitio.html"></a>Inicio</li>
+            <li><a href="creadores.html"></a>Sobre nosotros</li>
+        </ul>
+    </header>
+    <script>
+        
+    </script>
+</body>
+</html>
