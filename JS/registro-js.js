53099d3179daec75d4fae8b4e6eee449ae0d80c6
cirosrs <165937791+cirosrs@users.noreply.github.com>
2025-09-01 10:22:58 -0300

    correccion de errores con los links y el css

--- JS/registro-js.js
+++ JS/registro-js.js
@@ -1,3 +1,25 @@
 function inSesion() {
-    
+    document.getElementByClassName("login-button").addEventListener("click", () => {
+    const usDir = document.getElementByName("Usuario").value;
+    const contraDir = document.getElementByName("contraseña").value;
+    if (usDir && contraDir) {
+        fetch("agregarDirector.php", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/x-www-form-urlencoded"
+            },
+            body: "nombre=" + encodeURIComponent(usDir) + "&pais=" + encodeURIComponent(contraDir)
+        }) //GET
+            .then(res => res.json())
+            .then(res => {
+                if (res.error) {
+                    console.error(res.error + ": " + res.msj);
+                } else {
+                    console.log(res.msj);
+                }
+            });
+    } else {
+        console.log("Escribi algo");
+    }
+});
 }
