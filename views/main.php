77333bd3e21deda0e5dda921a1ac7a787910beae
SantinoRiojaZupo <santinoriojazupo@gmail.com>
2025-09-07 17:07:33 -0300

    hago la parte de main

--- views/main.php
+++ views/main.php
@@ -1,4 +1,22 @@
-
-            <div>
+<div>
                 <h1>Gacha-Dex</h1>
             </div>
+<main class="main-layout">
+    <div class="side-box left-box">
+        <h2>Breve explicación</h2>
+        <p>Aprete el boton de rolls para buscar un pokemon</p>
+    </div>
+    <div class="center-boxes">
+        <div class="center-box">
+            <h2>Posibilidad shiny/legendario:</h2>
+            <p id="probabilidad"><!--de tener Shiny/Legendario --></p>
+        </div>
+        <div class="center-box">
+            <button id="rolls">Roll</button>
+        </div>
+    </div>
+    <div class="side-box right-box">
+        <h2>Últimos PKMN conseguidos</h2>
+        <p id="pokemonesConseguidos"><!--Aca van a apareces los nombres o x cosa de los pokemons conseguidos--></p>
+    </div>
+</main>
