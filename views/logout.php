0d62f5a536c701d401c4902cc1b4a5df1bb288f0
Agustin Castro <agus.castro.sosa@gmail.com>
2025-09-18 22:44:11 -0300

    registrarse ahora redirige a inicar sesion despues de que el registro salga bien

--- views/logout.php
+++ views/logout.php
@@ -19,6 +19,6 @@ if (ini_get("session.use_cookies")) {
 session_destroy();
 
 // Redirige al usuario a la página de inicio de sesión o a la página principal
-header("Location: index.php");
+header("Location: index.php?page=main");
 exit();
 ?>
