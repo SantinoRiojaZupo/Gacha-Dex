2376b1b80e33fdafbe24f23404b8e3a738a45528
Thiago-Anzaldo <thiagoanzaldo008@gmail.com>
2025-10-08 20:10:23 -0300

    arreglo de css en inventario

--- views/inventarioHeader.php
+++ views/inventarioHeader.php
@@ -3,19 +3,6 @@
 <!DOCTYPE html>
 <html lang="es">
 <head>
-<style>
-  /* Esto evita que los estilos viejos del layout afecten */
-  body {
-    all: unset;
-    display: block;
-    margin: 0;
-    padding: 0;
-    font-family: 'PokemonGB', sans-serif;
-    background: linear-gradient(to bottom, #6fb1fc, #4364f7, #1e3c72);
-    min-height: 100vh;
-    color: white;
-  }
-</style>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Inventario Pokémon</title>
