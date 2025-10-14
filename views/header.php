736c9a6f07039afd88d4733372572cd073d0eab9
MarcosMartins1212 <marcosaliasrobertito@gmail.com>
2025-10-14 08:27:55 -0300

    arregle y modifique la barra de inicio de la pagina principal y modifique el php para que funcione

--- views/header.php
+++ views/header.php
@@ -1,12 +1,13 @@
 <?php
 
+
 require_once "../config/conexion.php";
 ?>
 <div id="barraInicio">
     <a href="index.php?page=main" class="logo">
         <img src="../imagenes/gachadex.png" alt="Pokeball" class="logo-img">
 </a>
-        
+       
     <div class="header-left">
         <button id="botonMenu">Menú</button>
     <ul id="menu">
@@ -16,7 +17,7 @@ require_once "../config/conexion.php";
         <li><a href="index.php?page=pokedex">pokedex</a></li>
         <li><a href="index.php?page=chat">Chat</a></li>
     </ul>
-    
+   
 </div>
     <div class="header-right">
     <?php if (isset($_SESSION['user_id'])): ?>
@@ -25,19 +26,31 @@ require_once "../config/conexion.php";
             <span class="saludo">¡Hola, <?php echo htmlspecialchars($_SESSION['username']); ?>!</span>
 
 
+
+
             <a href="../logout.php">Cerrar Sesión</a>
+
 
         </div>
     <?php else: ?>
     <button id="botonRegister">Registrate!</button>
     <button id="botonLogin">Inicie sesion!</button>
     <?php endif; ?>
+
+
+    <?php if (isset($_SESSION['user_id'])): ?>
     <div class="busqueda">
     <input type="search" id="buscadorUsuario" placeholder="Buscar...">
     <ul id="resultadosBusqueda">
 
+
     </ul>
     </div>
+
+
     <button id="buscarUsuario">Busca ya!</button>
+    <?php endif; ?>
+
+
 </div>
 </div>
