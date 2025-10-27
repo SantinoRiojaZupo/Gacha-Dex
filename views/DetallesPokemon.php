bd016deda167a311275f85e23881cb7894bc653f
Agustin Castro <agus.castro.sosa@gmail.com>
2025-10-27 08:42:56 -0300

    agrego funciones del botonMenu a detalles pokemon

--- views/DetallesPokemon.php
+++ views/DetallesPokemon.php
@@ -19,12 +19,17 @@
 
 <div class="detalles-page">
   <header class="header">
-    <button class="menu-btn">☰ Menu</button>
+    <button id="botonMenu">Menú</button>
+    <ul id="menu">
+      <li id="li1">Pagina Principal</li>
+      <li id="li2">Pokedex</a></li>
+      <li id="li3">Chat</a></li>
+    </ul>
     <div class="search-section">
       <input type="text" placeholder="Buscar Pokemon..." class="search-input">
       <button class="search-btn">Busca ya!</button>
     </div>
-    
+
     <link rel="stylesheet" href="../CSS/estilosDetallespokemon.css" />
   </header>
 
@@ -33,19 +38,23 @@
       <img id="0" src="" alt="pokemon">
     </div>
     <div class="pokemon-info">
-      <h1 class="pokemon-name" id="1" >Pokemon</h1>
+      <h1 class="pokemon-name" id="1">Pokemon</h1>
       <div class="pokemon-description" id="2">*Descripcion del pokemon*</div>
       <div class="pokemon-details">
-        <p><strong>Tipo:</strong> <span class="tag"id="3">*Tipo*</span></p>
-        <p><strong>Debilidades:</strong> <span class="tag"id="4">*Debilidades*</span></p>
-        <p><strong>Habilidad/es:</strong> <span class="tag"id="5">*Habilidad/es*</span></p>
-        <p><strong>Habilidad oculta:</strong> <span class="tag"id="6">*Habilidad oculta*</span></p>
+        <p><strong>Tipo:</strong> <span class="tag" id="3">*Tipo*</span></p>
+        <p><strong>Debilidades:</strong> <span class="tag" id="4">*Debilidades*</span></p>
+        <p><strong>Habilidad/es:</strong> <span class="tag" id="5">*Habilidad/es*</span></p>
+        <p><strong>Habilidad oculta:</strong> <span class="tag" id="6">*Habilidad oculta*</span></p>
       </div>
     </div>
   </div>
-  
+
 </div>
 <script>
-  const idpokemon = <?php echo json_encode($_GET['idpokemon']);?>;
+  const idpokemon = <?php echo json_encode($_GET['idpokemon']); ?>;
 </script>
-<script src="../JS/MostrarDatosPokemon.js"> 
+<script src="../JS/MostrarDatosPokemon.js">
+</script>
+<script src="../JS/botonMenu.js">
+</script>
+<script src="../JS/menuOpciones.js">
