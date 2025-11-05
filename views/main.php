a797cc2a5847a929d73a0909d702616abeede1c9
DarioBenjaminMontero <dariomontero2008@gmail.com>
2025-11-05 10:57:59 -0300

    cambios drasticos en el css de main

--- views/main.php
+++ views/main.php
@@ -2,6 +2,7 @@
                 <h1>Gacha-Dex</h1>
             </div>
 <main class="main-layout">
+    
     <div class="side-box left-box">
         <h2>Breve explicación</h2>
         <p>Aprete el boton de rolls para buscar un pokemon</p>
@@ -35,6 +36,16 @@
         <h2>Últimos PKMN conseguidos</h2>
         <div id="pokemonesConseguidos"><!--Aca van a apareces los nombres o x cosa de los pokemons conseguidos--></div>
     </div>
+ 
 </main>
+<script>
+const musica = new Audio('../sonidos/1-35.%20Vermilion%20City.mp3');
+musica.volume = 0.5;
+musica.loop = true;
+document.addEventListener('click', () => {
+  musica.play();
+});
+
+</script>
 <script src="../JS/botonPreguntas.js"></script>
 <script src="../JS/rolls.js"></script>
