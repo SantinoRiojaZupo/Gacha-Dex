5045fb3bc0097aa8cd070f1d668655cab1383109
SantinoRiojaZupo <santinoriojazupo@gmail.com>
2025-11-10 11:34:42 -0300

    actualizacion de estilos

--- config/conexion.php
+++ config/conexion.php
@@ -3,7 +3,7 @@ $server = "localhost";
 $user = "root";
 $pass = "";
 $bd = "gachadex";
-$conexion = mysqli_connect($server, $user, $pass, $bd, 3306);
+$conexion = mysqli_connect($server, $user, $pass, $bd);
 if (!$conexion) {
     echo json_encode(["error" => "No se pudo conectar a la base de datos"]);
     exit;
