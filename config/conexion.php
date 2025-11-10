74b06f6679e89e1b424ed5fa70537311e256ba00
SantinoRiojaZupo <santinoriojazupo@gmail.com>
2025-11-10 12:02:18 -0300

    Empiezo con las opciones de musica

--- config/conexion.php
+++ config/conexion.php
@@ -3,7 +3,7 @@ $server = "localhost";
 $user = "root";
 $pass = "";
 $bd = "gachadex";
-$conexion = mysqli_connect($server, $user, $pass, $bd);
+$conexion = mysqli_connect($server, $user, $pass, $bd,3307);
 if (!$conexion) {
     echo json_encode(["error" => "No se pudo conectar a la base de datos"]);
     exit;
--- views/footer.php
+++ views/footer.php
@@ -3,6 +3,13 @@
 <div id="footerControls">
   <button id="toggleMusic">🔊</button>
   <input id="musicVolume" type="range" min="0" max="1" step="0.01">
+  <select id=musicaSelect>
+                    <option value="0" selected>Default</option>
+                <option value="1">Vermilion City</option>
+                <option value="2">Pewter City</option>
+                <option value="3">Slateport City</option>
+                <option value="4">pokemon Center</option>
+                </select>
 </div>
 <div id="barraFinal">
     <p><a href="../views/index.php?page=creadores">Sobre nosotros</a></p>
